2d5334ef527a631a1354ebf02f726267747ffddf
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2021-01-19 20:29:17 +0100

    Implement dynamic values

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { AuthContext, useAuthState } from '../context/AuthContext';
 
 function Header() {
   const history = useHistory();
 
+  // context dingen
+  const { isAuthenticated } = useAuthState();
+  const { logout } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (isAuthenticated === false) {
+      history.push('/signin');
+    }
+  }, [isAuthenticated]);
+
   return (
     <header>
       <div>
-        <button
-          type="button"
-          onClick={() => history.push('/signin')}
-        >
-          Log in
-        </button>
-        <button
-          type="button"
-          onClick={() => history.push('/signup')}
-        >
-          Registreren
-        </button>
+        {isAuthenticated ? (
+          <button
+            type="button"
+            // Roep hier de logout functie uit de context aan!
+            onClick={() => logout()}
+          >
+            Log uit
+          </button>
+        ) : (
+          <>
+          <button
+            type="button"
+            onClick={() => history.push('/signin')}
+          >
+            Log in
+          </button>
+          <button
+            type="button"
+            onClick={() => history.push('/signup')}
+          >
+            Registreren
+          </button>
+          </>
+        )}
       </div>
     </header>
   );
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuthState } from '../context/AuthContext';
 
 function Profile() {
+  const { user } = useAuthState();
+
   return (
     <>
       <h1>Profielpagina</h1>
       <h2>Gegevens</h2>
-      <p><strong>Gebruikersnaam:</strong> novaeeken</p>
-      <p><strong>Email:</strong> nova@gmail.com</p>
+      {user && (
+        <>
+          <p><strong>Gebruikersnaam:</strong> {user.username}</p>
+          <p><strong>Email:</strong> {user.email}</p>
+        </>
+      )}
+
       <h2>Afgeschermde content voor ingelogde gebruikers</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias
+        qui quo unde?</p>
       <p>Terug naar de <Link to="/">Homepagina</Link></p>
     </>
   );
