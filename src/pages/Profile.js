1c3d1d858b6c64c955f2c92cb0381d56b5fe20c4
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2021-01-29 05:24:46 +0100

    Create authenticated request

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuthState } from '../context/AuthContext';
+import axios from 'axios';
 
 function Profile() {
   const { user } = useAuthState();
 
-  // Wil je beschermde data uitlezen?
-  // Dan zet je hier weer een useEffect met lege [] dependency array
-  // asynchrone functie met try/catch
-  // Maar in het request stuur je de token die in de local storage staat, mee!
+  // State voor de data-status
+  const [error, setError] = useState('');
+  const [protectedData, setProtectedData] = useState('');
+
+  useEffect(() => {
+    async function getProtectedData() {
+      setError('');
+      try {
+        // haal de token op uit de local storage
+        const token = localStorage.getItem('token');
+
+        // haal de protected data op met de token meegestuurd
+        const response = await axios.get('http://localhost:8080/api/test/user', {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          }
+        });
+
+        // zet deze data in de state zodat we dit in het component kunnen laten zien
+        setProtectedData(response.data);
+      } catch(e) {
+        setError('Er is iets misgegaan bij het ophalen van de data')
+      }
+    }
+
+    getProtectedData();
+  }, []);
 
   return (
     <>
       <h1>Profielpagina</h1>
       <h2>Gegevens</h2>
-      {user && (
-        <>
-          <p><strong>Gebruikersnaam:</strong> {user.username}</p>
-          <p><strong>Email:</strong> {user.email}</p>
-        </>
-      )}
-
+      {user &&
+      <>
+        <p><strong>Gebruikersnaam: </strong>{user.username}</p>
+        <p><strong>Email: </strong>{user.email}</p>
+      </>
+      }
       <h2>Afgeschermde content voor ingelogde gebruikers</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias
-        qui quo unde?</p>
+      {protectedData && <p>{protectedData}</p>}
+      {error && <p className="message-error">{error}</p>}
       <p>Terug naar de <Link to="/">Homepagina</Link></p>
     </>
   );
