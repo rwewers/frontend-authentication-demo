048aadbf211c8e8c811d3b255120b8991fc4e492
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2021-01-29 05:31:54 +0100

    Keep data on refresh

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 const AuthContext = createContext({});
 
@@ -10,18 +11,56 @@ function AuthContextProvider({ children }) {
   })
 
   useEffect(() => {
-    // haal uit de local storage de JWT Token
-    // Als die er niet is, kunnen we gewoon verder
-    // Als die token er wel is, dan betekend dat dat de applicatie herstart is
-    // En dan willen we nog even onze gebruikersdata (username, etc.) ophalen.
+    const token = localStorage.getItem('token');
 
-    setTimeout(() => {
-      // er is geen token, dus we beginnen met een schone lei!
+    async function getUserInfo() {
+      try {
+        // We kunnen de gebruikersdata ophalen omdat we onszelf authenticeren met de token
+        const response = await axios.get('https://polar-lake-14365.herokuapp.com/api/user', {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        console.log(response);
+
+        // met het resultaat vullen we de context
+        setAuthState({
+          ...authState,
+          user: {
+            id: response.id,
+            username: response.username,
+            email: response.email,
+          },
+          status: 'done',
+        });
+
+      } catch (e) {
+        // Gaat er toch iets mis? Dan zetten we de error in de context
+        setAuthState({
+          ...authState,
+          user: null,
+          error: e,
+          status: 'done'
+        });
+      }
+    }
+
+    // als we GEEM userinformatie meer in de applicatie hebben, maar er staat WEL een token in
+    // local storage, gaan we handmatig de gebuikersdata ophalen door de getUserInfo functie van hierboven aan te roepen
+    if (authState.user === null && token) {
+      getUserInfo();
+    } else {
+      // Als er geen ingelogde gebruiker hoeft te zijn, zetten we de context naar de basis state
       setAuthState({
         ...authState,
-        status: 'done',
-      })
-    }, 2000)
+        error: null,
+        user: null,
+        status: 'done'
+      });
+    }
   }, []);
 
   function login(data) {
